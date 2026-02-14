3ecda34605436086a2ff6986129186fc559f5b50
John5Cruz <john._l@hotmail.com>
2026-02-14 00:40:29 -0600

    multiple products included in tracking

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 import undetected_chromedriver as ch
 import pandas as pd
@@ -15,26 +16,29 @@ import os
 import sys
 import numpy as np
 
+products = {'motorcycle': "https://www.mercadolibre.com.mx/motocicleta-chopper-italika-tc-300-negra/up/MLMU3007051693",
+            'ram': "https://www.mercadolibre.com.mx/memoria-ram-ddr5-16gb-5600mts-kingston-fury-impact-1x16gb-laptop-negro-kf556s40ib-16/p/MLM25390982"}
+
+
 # link for the product to be monitored
 ruta = "https://www.mercadolibre.com.mx/motocicleta-chopper-italika-tc-300-negra/up/MLMU3007051693"
 ruta2 = "https://www.amazon.com.mx/dp/B07G7CHRQY/?coliid=I3MMYSO5HGSFQM&colid=3AZBLI2SFHTWM&ref_=list_c_wl_lv_ov_lig_dp_it&th=1&psc=1"
 
 
-def save_data(date: str, price: int):   # save the data in a database file
+def save_data(product: str, date: str, price: int):   # save the data in a database file
     data = os.path.join(os.path.dirname(__file__), 'datos.db')  # it is required to define absolute path
     connection = sqlite3.connect(data)
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS motocicleta(fecha TEXT,precio INTEGER)")
-    cursor.execute("INSERT INTO motocicleta (fecha, precio) VALUES (?, ?)", (date, price))
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {product}(fecha TEXT,precio INTEGER)")
+    cursor.execute(f"INSERT INTO {product} (fecha, precio) VALUES (?, ?)", (date, price))
     connection.commit()
     connection.close()
 
 
-def graph_data():
+def graph_data(table):
     data = os.path.join(os.path.dirname(__file__), 'datos.db')
     conn = sqlite3.connect(data)
 
-    table = 'motocicleta'
     query = f"SELECT * FROM {table}"
     df = pd.read_sql_query(query, conn)
     conn.close()
@@ -82,8 +86,9 @@ class BasePage:
     def find_element(self, method, name):
         try:
             return self.__wait.until(EC.presence_of_element_located((method, name)))
-        except TimeoutError:
+        except TimeoutException:
             print(f"Element {name} is not found")
+            return None
 
     def close_page(self):
         self.__driver.close()
@@ -99,7 +104,10 @@ class MercadoLibrePage(BasePage):
     def get_price(self):
         super().open_page(self.link)
         text_price = super().find_element(self.__locator, self.__name)
-        return text_price.text.split('\n')[1].replace(',', '')
+        if text_price is None:
+            return None
+        else:
+            return text_price.text.split('\n')[1].replace(',', '')
 
 
 class AmazonPage(BasePage):
@@ -115,17 +123,24 @@ class AmazonPage(BasePage):
 
 if __name__ == '__main__':
     #open_webpages()
-    #graph_data()
+    #graph_data(list(products.keys())[0])
+    #sys.exit()
+
+    items = list(products.keys())
     options = webdriver.ChromeOptions()
     driver = ch.Chrome(options=options, version_main=144)
     driver.maximize_window()
 
-    pag1 = BasePage(driver, ruta2)
-    price = pag1.get_price()
+    for item in items:
+        pag1 = BasePage(driver, products[item])
+        price = pag1.get_price()
+        moment = datetime.datetime.now()
+        date = str(moment).split('.')[0]
+        if price is not None:
+            print(f"For {item} in {date} the price is ${price}")
+            save_data(item, date, int(price))
+        else:
+            print(f"Product {item} not available")
+        time.sleep(3)
     pag1.close_browser()
-
-    moment = datetime.datetime.now()
-    date = str(moment).split('.')[0]
-    print(f"Para la fecha {date} el precio obtenido es {price}")
-    #save_data(date, int(price))
 
